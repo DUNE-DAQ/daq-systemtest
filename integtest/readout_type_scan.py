44b3a5be697cc68e2cf356c3f58f56a14acaf4eb
Alessandro Thea <alessandro.thea@cern.ch>
2024-10-28 10:49:08 +0100

    Removed SW from TPG

--- integtest/readout_type_scan.py
+++ integtest/readout_type_scan.py
@@ -133,12 +133,12 @@ conf_dict.config_substitutions.append(
     )
 )
 
-swtpg_conf = copy.deepcopy(conf_dict)
-swtpg_conf.tpg_enabled = True
-swtpg_conf.frame_file = (
+tpg_conf = copy.deepcopy(conf_dict)
+tpg_conf.tpg_enabled = True
+tpg_conf.frame_file = (
     "asset://?checksum=dd156b4895f1b06a06b6ff38e37bd798"  # WIBEth All Zeros
 )
-swtpg_conf.config_substitutions.append(
+tpg_conf.config_substitutions.append(
     data_classes.config_substitution(
         obj_class="TAMakerPrescaleAlgorithm",
         obj_id="dummy-ta-maker",
@@ -195,7 +195,7 @@ pds_conf.config_substitutions.append(
 confgen_arguments = {
     # "WIB1_System": wib1_conf,
     "WIBEth_System": wibeth_conf,
-    "Software_TPG_System": swtpg_conf,
+    "TPG_System": tpg_conf,
     "PDS_Stream_System": pds_stream_conf,
     # "PDS_System": pds_conf,
     "TDEEth_System": tde_conf,
